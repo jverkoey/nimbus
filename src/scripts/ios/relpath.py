502ce4dc0d175095cbcc5f802fbcd8af046606da
jverkoey <jverkoey@gmail.com>
2011-06-07 17:38:39 -0400

    Add nimbus scripts.

---
+++ src/scripts/ios/relpath.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+relpath.py
+
+A method for calculating the relative path from one directory to another.
+
+Originally written by Cimarron Taylor:
+http://code.activestate.com/recipes/208993-compute-relative-path-from-one-directory-to-anothe/
+Date: July 6, 2003
+
+Modifications Copyright 2011 Jeff Verkoeyen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+def pathsplit(p, rest=[]):
+    """ This version, in contrast to the original version, permits trailing
+    slashes in the pathname (in the event that it is a directory).
+    It also uses no recursion """
+    return path.split(os.path.sep)
+
+def commonpath(l1, l2, common=[]):
+    if len(l1) < 1: return (common, l1, l2)
+    if len(l2) < 1: return (common, l1, l2)
+    if l1[0] != l2[0]: return (common, l1, l2)
+    return commonpath(l1[1:], l2[1:], common+[l1[0]])
+
+def relpath(p1, p2):
+    (common,l1,l2) = commonpath(pathsplit(p1), pathsplit(p2))
+    p = []
+    if len(l1) > 0:
+        p = [ '../' * len(l1) ]
+    p = p + l2
+    return os.path.join( *p )
