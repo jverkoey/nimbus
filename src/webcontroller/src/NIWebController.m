bb5c3e16f64913e9f779808cb41e32cd46f616f2
Roger Chapman <roger@stormid.com>
2011-07-29 17:30:17 +0100

    Change licence to Apache 2.0

--- src/webcontroller/src/NIWebController.m
+++ src/webcontroller/src/NIWebController.m
@@ -1,6 +1,8 @@
 //
 // Copyright 2011 Roger Chapman
 //
+// Forked from Three20 July 29, 2011 - Copyright 2009-2011 Facebook
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
