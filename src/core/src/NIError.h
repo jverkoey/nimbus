8691a0a009fe39872c756c8aeb06887a584dc18b
jverkoey <jverkoey@gmail.com>
2011-08-01 06:53:39 -0700

    [core] Add NIError.h/m.

---
+++ src/core/src/NIError.h
@@ -0,0 +1,43 @@
+//
+// Copyright 2011 Jeff Verkoeyen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+/**
+ * For defining various error types used throughout the Nimbus framework.
+ *
+ * @ingroup NimbusCore
+ * @defgroup Errors Errors
+ * @{
+ */
+
+/** The Nimbus error domain. */
+extern NSString* const NINimbusErrorDomain;
+
+/** The key used for images in the NSError userInfo. */
+extern NSString* const NIImageErrorKey;
+
+/** NSError codes in NINimbusErrorDomain. */
+enum {
+  /** The image is too small to be used. */
+  NIImageTooSmall = 1,
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**@}*/// End of Errors ///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
