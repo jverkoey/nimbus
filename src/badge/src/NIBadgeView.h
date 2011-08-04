1fbcdefc50bdf266393fa720900923f521402100
Roger Chapman <roger@stormid.com>
2011-08-04 15:26:49 +0100

    Allows for custom text color and added a little bit more documentation

--- src/badge/src/NIBadgeView.h
+++ src/badge/src/NIBadgeView.h
@@ -16,14 +16,33 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * A styled view that mimics the iOS badge on a app icon.
+ *
+ *      @ingroup Badge-Views
+ *
+ * This style comprises of four elements: the main colored background, the
+ * white border, the gloss effect and a drop shadow. The tint color of the
+ * view is customizable, but defaults to to the standard red. 
+ 
+ * This view is drawn using Quartz which means there are no loading of 
+ * streachable images, or laying out subviews; thus creating a highly 
+ * performant view. 
+ */
 @interface NIBadgeView : UIView {
   NSString* _text;
   UIColor* _tintColor;
   UIFont* _font;
+  UIColor* _textColor;
 }
 
 /*
  * The text to display within the badge.
+ *
+ *      @attention  If you want the view to rezise based on the text be sure
+ *                  be sure to call -sizeToFit after you set the text, as this 
+ *                  will not happen automatically.
+       
  */
 @property (nonatomic, readwrite, copy) NSString* text;
 
@@ -35,9 +54,19 @@
 
 
 /*
- * The font of the text within the badge. The default is the bold system
- * font of size 13
+ * The font of the text within the badge. 
+ * The default is bold system font of size 13.
+ *
+ *      @attention  If you want the view to rezise based on the font size be 
+ *                  sure be sure to call -sizeToFit after you set the text, as  
+ *                  this will not happen automatically.
  */
 @property (nonatomic, readwrite, assign) UIFont* font;
+
+
+/*
+ * The text color for the badge. The default color is white.
+ */
+@property (nonatomic, readwrite, assign) UIColor* textColor;
 
 @end
--- src/badge/src/NIBadgeView.m
+++ src/badge/src/NIBadgeView.m
@@ -24,6 +24,7 @@ static const CGFloat kBadgeLineSize = 2.0;
 @synthesize text      = _text;
 @synthesize tintColor = _tintColor;
 @synthesize font      = _font;
+@synthesize textColor = _textColor;
 
 - (id) initWithFrame:(CGRect)frame {
   self = [super initWithFrame:frame];
@@ -31,7 +32,8 @@ static const CGFloat kBadgeLineSize = 2.0;
     self.contentScaleFactor = NIScreenScale();
 		self.backgroundColor = [UIColor clearColor];
     self.tintColor = [UIColor redColor];
-    self.font = [UIFont boldSystemFontOfSize:13];
+    self.font = [UIFont boldSystemFontOfSize:14];
+    self.textColor = [UIColor whiteColor];
   }
   return self;
 }
@@ -41,15 +43,15 @@ static const CGFloat kBadgeLineSize = 2.0;
 	CGFloat rectWidth, rectHeight;
 	CGSize stringSize = [self.text sizeWithFont:_font];
   
-  CGFloat scaleFactor = _font.pointSize / 13;
+  CGFloat scaleFactor = _font.pointSize / 14;
   
 	if ([self.text length]>=2) {
 		rectWidth = 10 + (stringSize.width + [self.text length]); 
-    rectHeight = 22;
+    rectHeight = 25;
 		
     return CGSizeMake(rectWidth, rectHeight * scaleFactor);
 	}
-  return CGSizeMake(22 * scaleFactor, 22 * scaleFactor);
+  return CGSizeMake(25 * scaleFactor, 25 * scaleFactor);
 }
 
 -(void)setText:(NSString *)text {
@@ -64,6 +66,11 @@ static const CGFloat kBadgeLineSize = 2.0;
 
 -(void)setTintColor:(UIColor *)tintColor {
   _tintColor = tintColor;
+  [self setNeedsDisplay];
+}
+
+-(void)setTextColor:(UIColor *)textColor {
+  _textColor = textColor;
   [self setNeedsDisplay];
 }
 
@@ -91,6 +98,36 @@ static const CGFloat kBadgeLineSize = 2.0;
   
   CGContextRestoreGState(context);
   
+  //Add the gloss effect
+  CGContextSaveGState(context);
+  
+  CGContextBeginPath(context);
+  CGContextAddArc(context, maxX-radius, minY+radius, radius, M_PI+(M_PI/2), 0, 0);
+	CGContextAddArc(context, minX+radius, minY+radius, radius, M_PI, M_PI+M_PI/2, 0);
+  // Gloss should have a bottom curve. Math anyone?
+	CGContextClip(context);
+	
+	size_t num_locations = 2;
+	CGFloat locations[2] = { 0.0, 0.3 };
+	CGFloat components[8] = {  0.92, 1.0, 1.0, 1.0, 0.82, 0.82, 0.82, 0.3 };
+  
+	CGColorSpaceRef cspace;
+	CGGradientRef gradient;
+	cspace = CGColorSpaceCreateDeviceRGB();
+	gradient = CGGradientCreateWithColorComponents (cspace, components, locations, num_locations);
+	
+	CGPoint sPoint, ePoint;
+	sPoint.x = 0;
+	sPoint.y = 1;
+	ePoint.x = 0;
+	ePoint.y = maxY;
+	CGContextDrawLinearGradient (context, gradient, sPoint, ePoint, 0);
+	
+	CGColorSpaceRelease(cspace);
+	CGGradientRelease(gradient);
+  
+  CGContextRestoreGState(context);
+  
   // Draw the border
   CGContextBeginPath(context);
   CGContextSetLineWidth(context, kBadgeLineSize);
@@ -103,47 +140,14 @@ static const CGFloat kBadgeLineSize = 2.0;
 	CGContextClosePath(context);
 	CGContextStrokePath(context);
   
-  //Add the gloss effect
-  CGContextSaveGState(context);
-  
-  CGContextBeginPath(context);
-	CGContextAddArc(context, maxX-radius, minY+radius, radius, M_PI+(M_PI/2), 0, 0);
-	CGContextAddArc(context, maxX-radius, (maxY-radius)/3, radius, 0, M_PI/2, 0);
-	CGContextAddArc(context, minX+radius, (maxY-radius)/3, radius, M_PI/2, M_PI, 0);
-	CGContextAddArc(context, minX+radius, minY+radius, radius, M_PI, M_PI+M_PI/2, 0);
-	CGContextClip(context);
-	
-	
-	size_t num_locations = 2;
-	CGFloat locations[2] = { 0.0, 0.4 };
-	CGFloat components[8] = {  0.92, 1.0, 1.0, 1.0, 0.82, 0.82, 0.82, 0.2 };
-  
-	CGColorSpaceRef cspace;
-	CGGradientRef gradient;
-	cspace = CGColorSpaceCreateDeviceRGB();
-	gradient = CGGradientCreateWithColorComponents (cspace, components, locations, num_locations);
-	
-	CGPoint sPoint, ePoint;
-	sPoint.x = 0;
-	sPoint.y = 0;
-	ePoint.x = 0;
-	ePoint.y = maxY;
-	CGContextDrawLinearGradient (context, gradient, sPoint, ePoint, 0);
-	
-	CGColorSpaceRelease(cspace);
-	CGGradientRelease(gradient);
-  
-  CGContextRestoreGState(context);
-  
   // Draw text
-  // Should this be customizable?
-  [[UIColor whiteColor] set];
+  [self.textColor set];
   CGSize textSize = [self.text sizeWithFont:self.font];
   
   // We remove 1 point from the y-axis to account for the drop shadow.
   [self.text drawAtPoint:
    CGPointMake(floorf(rect.size.width/2-textSize.width/2), 
-               floorf(rect.size.height/2-textSize.height/2-1)) 
+               floorf(rect.size.height/2-textSize.height/2-2)) 
                 withFont:self.font];
   
   }
