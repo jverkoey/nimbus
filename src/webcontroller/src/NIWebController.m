63b53ac25c8d1d2cd65efd31ba504662f5168e48
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-08-12 10:37:12 +0200

    Fix issue #36. Implement "copy this URL" in NIWebController.

--- src/webcontroller/src/NIWebController.m
+++ src/webcontroller/src/NIWebController.m
@@ -100,11 +100,11 @@
 
   if (nil == _actionSheet) {
     _actionSheet =
-    [[UIActionSheet alloc] initWithTitle: nil
-                                delegate: self
-                       cancelButtonTitle: NSLocalizedString(@"Cancel", @"")
-                  destructiveButtonTitle: nil
-                       otherButtonTitles: NSLocalizedString(@"Open in Safari", @""), nil];
+    [[UIActionSheet alloc] initWithTitle:[self.URL absoluteString]
+                                delegate:self
+                       cancelButtonTitle:NSLocalizedString(@"Cancel", @"")
+                  destructiveButtonTitle:nil
+                       otherButtonTitles:NSLocalizedString(@"Open in Safari", @""), NSLocalizedString(@"Copy URL", @""), nil];
   }
 
   if (NIIsPad()) {
@@ -336,6 +336,9 @@
 - (void)actionSheet:(UIActionSheet*)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
   if (buttonIndex == 0) {
     [[UIApplication sharedApplication] openURL:self.URL];
+  }
+  else if (buttonIndex == 1) {
+    [[UIPasteboard generalPasteboard] setURL:self.URL];
   }
 }
 
