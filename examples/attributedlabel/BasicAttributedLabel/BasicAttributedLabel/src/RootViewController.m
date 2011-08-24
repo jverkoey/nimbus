7ad1d551e9a6d2f4c5eea58aa7b7580de74eb9ab
Roger Chapman <roger@stormid.com>
2011-08-24 11:23:47 +0100

    Refactor to pull out some common methods

--- examples/attributedlabel/BasicAttributedLabel/BasicAttributedLabel/src/RootViewController.m
+++ examples/attributedlabel/BasicAttributedLabel/BasicAttributedLabel/src/RootViewController.m
@@ -1,9 +1,17 @@
 //
-//  RootViewController.m
-//  BasicAttributedLabel
+// Copyright 2011 Roger Chapman
 //
-//  Created by Roger Chapman on 23/08/2011.
-//  Copyright 2011 Storm ID Ltd. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #import "RootViewController.h"
@@ -24,6 +32,8 @@
 }
 
 -(void)viewDidLoad {
+  
+  self.title = @"NIAttributedLabel";
 
   label2.textAlignment = UITextAlignmentJustify;
 }
--- src/attributedlabel/src/NIAttributedLabel.m
+++ src/attributedlabel/src/NIAttributedLabel.m
@@ -47,26 +47,11 @@
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
--(void) resetFromLabel {
-  NSMutableAttributedString* attibutedString = self.text ? 
-  [[[NSMutableAttributedString alloc] initWithString:self.text] autorelease] : nil;
+-(void) attributedString:(NSMutableAttributedString*)attributedString
+        setTextAlignment:(CTTextAlignment)textAlignment 
+           lineBreakMode:(CTLineBreakMode)lineBreakMode {
   
-  NSRange range = NSMakeRange(0,[attibutedString length]);
-  
-  CTFontRef font = CTFontCreateWithName((CFStringRef)self.font.fontName, self.font.pointSize, nil);
-  // Fix for Apple leak
-  [attibutedString removeAttribute:(NSString * )kCTFontAttributeName range:range]; 
-	[attibutedString addAttribute:(NSString*)kCTFontAttributeName value:(id)font range:range];
-	CFRelease(font);
-  
-  // Fix for Apple leak
-  [attibutedString removeAttribute:(NSString * )kCTForegroundColorAttributeName range:range]; 
-	[attibutedString addAttribute:(NSString*)kCTForegroundColorAttributeName 
-                          value:(id)self.textColor.CGColor 
-                          range:range];
-  
-  CTTextAlignment textAlignment = [self alignmentFromUITextAlignment:self.textAlignment];
-  CTLineBreakMode lineBreak = [self lineBreakModeFromUILineBreakMode:self.lineBreakMode];
+  NSRange range = NSMakeRange(0,[attributedString length]);
   
   CTParagraphStyleSetting paragraphStyles[2] = {
 		{.spec = kCTParagraphStyleSpecifierAlignment, 
@@ -74,15 +59,52 @@
       .value = (const void*)&textAlignment},
 		{.spec = kCTParagraphStyleSpecifierLineBreakMode, 
       .valueSize = sizeof(CTLineBreakMode), 
-      .value = (const void*)&lineBreak},
+      .value = (const void*)&lineBreakMode},
 	};
 	CTParagraphStyleRef style = CTParagraphStyleCreate(paragraphStyles, 2);
-  [attibutedString addAttribute:(NSString*)kCTParagraphStyleAttributeName 
+  [attributedString addAttribute:(NSString*)kCTParagraphStyleAttributeName 
                           value:(id)style 
                           range:range];
   CFRelease(style);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(void) attributedString:(NSMutableAttributedString*)attributedString
+            setTextColor:(UIColor*)color{
   
-  self.attributedText = attibutedString;
+  NSRange range = NSMakeRange(0,[attributedString length]);
+  [attributedString removeAttribute:(NSString *)kCTForegroundColorAttributeName range:range]; 
+	[attributedString addAttribute:(NSString*)kCTForegroundColorAttributeName 
+                           value:(id)color.CGColor 
+                           range:range];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(void) attributedString:(NSMutableAttributedString*)attributedString
+            setFont:(UIFont*)font{
+  
+  NSRange range = NSMakeRange(0,[attributedString length]);
+  CTFontRef fontRef = CTFontCreateWithName((CFStringRef)font.fontName, font.pointSize, nil);
+  [attributedString removeAttribute:(NSString*)kCTFontAttributeName range:range]; 
+	[attributedString addAttribute:(NSString*)kCTFontAttributeName value:(id)fontRef range:range];
+	CFRelease(font);
+  
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(void) resetFromLabel {
+  NSMutableAttributedString* attributedString = self.text ? 
+  [[[NSMutableAttributedString alloc] initWithString:self.text] autorelease] : nil;
+  
+  [self attributedString:attributedString setFont:self.font];
+  [self attributedString:attributedString setTextColor:self.textColor];
+  
+  CTTextAlignment textAlignment = [self alignmentFromUITextAlignment:self.textAlignment];
+  CTLineBreakMode lineBreak = [self lineBreakModeFromUILineBreakMode:self.lineBreakMode];
+  [self attributedString:attributedString setTextAlignment:textAlignment lineBreakMode:lineBreak]; 
+  
+  self.attributedText = attributedString;
 }
 
 -(void)awakeFromNib {
@@ -113,11 +135,31 @@
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
--(void)setTextAlignment:(UITextAlignment)textAlignment{
+-(void)setTextAlignment:(UITextAlignment)textAlignment {
+  CTTextAlignment alignment = [self alignmentFromUITextAlignment:textAlignment];
+  CTLineBreakMode lineBreak = [self lineBreakModeFromUILineBreakMode:self.lineBreakMode];
+  [self attributedString:_attributedText setTextAlignment:alignment lineBreakMode:lineBreak]; 
   [super setTextAlignment:textAlignment];
-  
-  // TEMP: we don't want to call resetFromLabel: here because we may have set attributedText already
-  [self resetFromLabel];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(void)setLineBreakMode:(UILineBreakMode)lineBreakMode {
+  CTTextAlignment alignment = [self alignmentFromUITextAlignment:self.textAlignment];
+  CTLineBreakMode lineBreak = [self lineBreakModeFromUILineBreakMode:lineBreakMode];
+  [self attributedString:_attributedText setTextAlignment:alignment lineBreakMode:lineBreak]; 
+  [super setLineBreakMode:lineBreakMode];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(void)setTextColor:(UIColor *)textColor{
+  [self attributedString:_attributedText setTextColor:textColor];
+  [super setTextColor:textColor];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(void)setFont:(UIFont *)font {
+  [self attributedString:_attributedText setFont:font];
+	[super setFont:font];
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
