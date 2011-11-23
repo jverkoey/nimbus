9911b610e4434c67feb595a7be0ed593f289e1f7
jverkoey <jverkoey@gmail.com>
2011-11-23 12:40:04 -0800

    [core] Deprecate NIRectInset in favor of UIEdgeInsetsInsetRect.
    Closes #100.

--- src/models/src/NIFormCellCatalog.m
+++ src/models/src/NIFormCellCatalog.m
@@ -266,7 +266,7 @@
 - (void)layoutSubviews {
   [super layoutSubviews];
 
-  _textField.frame = NIRectInset(self.contentView.bounds, NICellContentPadding());
+  _textField.frame = UIEdgeInsetsInsetRect(self.contentView.bounds, NICellContentPadding());
 }
 
 
@@ -340,7 +340,7 @@
   [super layoutSubviews];
 
   UIEdgeInsets contentPadding = NICellContentPadding();
-  CGRect contentFrame = NIRectInset(self.contentView.frame, contentPadding);
+  CGRect contentFrame = UIEdgeInsetsInsetRect(self.contentView.frame, contentPadding);
 
   [_switchControl sizeToFit];
   CGRect frame = _switchControl.frame;
