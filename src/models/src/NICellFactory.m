6f7a6f3ada6f73e2c4ab12435b1ff83364cfee07
jverkoey <jverkoey@gmail.com>
2011-11-23 17:02:43 -0800

    [models] Add mapObjectClass:toCellClass: to NICellFactory.
    This allows for explicit mappings of objects to cells.

--- src/models/src/NICellFactory.m
+++ src/models/src/NICellFactory.m
@@ -16,91 +16,109 @@
 
 #import "NICellFactory.h"
 
+#import "NimbusCore.h"
+
+@interface NICellFactory()
+@property (nonatomic, readwrite, copy) NSMutableDictionary* objectToCellMap;
+@end
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 @implementation NICellFactory
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-+ (UITableViewCell *)tableViewModel: (NITableViewModel *)tableViewModel
-                   cellForTableView: (UITableView *)tableView
-                        atIndexPath: (NSIndexPath *)indexPath
-                         withObject: (id)object {
-  UITableViewCell* cell = nil;
-
-  // Only NICellObject-conformant objects may pass.
-  if ([object respondsToSelector:@selector(cellClass)]) {
-    Class cellClass = [object cellClass];
-    NSString* identifier = NSStringFromClass(cellClass);
-
-    cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-
-    if (nil == cell) {
-      UITableViewCellStyle style = UITableViewCellStyleDefault;
-      if ([object respondsToSelector:@selector(cellStyle)]) {
-        style = [object cellStyle];
-      }
-      cell = [[[cellClass alloc] initWithStyle:style reuseIdentifier:identifier] autorelease];
-    }
-
-    // Allow the cell to configure itself with the object's information.
-    if ([cell respondsToSelector:@selector(shouldUpdateCellWithObject:)]) {
-      [(id<NICell>)cell shouldUpdateCellWithObject:object];
-    }
-  }
-
-  return cell;
-}
-
-
-@end
+@synthesize objectToCellMap = _objectToCellMap;
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
-@interface NICellObject()
-@property (nonatomic, readwrite, assign) Class cellClass;
-@property (nonatomic, readwrite, retain) id userInfo;
-@end
+- (void)dealloc {
+  NI_RELEASE_SAFELY(_objectToCellMap);
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
-@implementation NICellObject
-
-@synthesize cellClass = _cellClass;
-@synthesize userInfo = _userInfo;
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-+ (id)objectWithCellClass:(Class)cellClass userInfo:(id)userInfo {
-  return [[[self alloc] initWithCellClass:cellClass userInfo:userInfo] autorelease];
+  [super dealloc];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-+ (id)objectWithCellClass:(Class)cellClass {
-  return [[[self alloc] initWithCellClass:cellClass userInfo:nil] autorelease];
-}
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-- (id)initWithCellClass:(Class)cellClass userInfo:(id)userInfo {
+- (id)init {
   if ((self = [super init])) {
-    self.cellClass = cellClass;
-    self.userInfo = userInfo;
+    _objectToCellMap = [[NSMutableDictionary alloc] init];
   }
   return self;
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-- (id)initWithCellClass:(Class)cellClass {
-  return [self initWithCellClass:cellClass userInfo:nil];
++ (UITableViewCell *)cellWithClass:(Class)cellClass
+                         tableView:(UITableView *)tableView
+                            object:(id)object {
+  UITableViewCell* cell = nil;
+
+  NSString* identifier = NSStringFromClass(cellClass);
+
+  cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+
+  if (nil == cell) {
+    UITableViewCellStyle style = UITableViewCellStyleDefault;
+    if ([object respondsToSelector:@selector(cellStyle)]) {
+      style = [object cellStyle];
+    }
+    cell = [[[cellClass alloc] initWithStyle:style reuseIdentifier:identifier] autorelease];
+  }
+
+  // Allow the cell to configure itself with the object's information.
+  if ([cell respondsToSelector:@selector(shouldUpdateCellWithObject:)]) {
+    [(id<NICell>)cell shouldUpdateCellWithObject:object];
+  }
+
+  return cell;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
++ (UITableViewCell *)tableViewModel:(NITableViewModel *)tableViewModel
+                   cellForTableView:(UITableView *)tableView
+                        atIndexPath:(NSIndexPath *)indexPath
+                         withObject:(id)object {
+  UITableViewCell* cell = nil;
+
+  // Only NICellObject-conformant objects may pass.
+  if ([object respondsToSelector:@selector(cellClass)]) {
+    Class cellClass = [object cellClass];
+    cell = [self cellWithClass:cellClass tableView:tableView object:object];
+  }
+
+  return cell;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (UITableViewCell *)tableViewModel: (NITableViewModel *)tableViewModel
+                   cellForTableView: (UITableView *)tableView
+                        atIndexPath: (NSIndexPath *)indexPath
+                         withObject: (id)object {
+  UITableViewCell* cell = nil;
+
+  Class objectClass = [object class];
+  Class cellClass = [self.objectToCellMap objectForKey:objectClass];
+
+  // Explicit mappings override implicit mappings.
+  if (nil != cellClass) {
+    cell = [[self class] cellWithClass:cellClass tableView:tableView object:object];
+
+  } else {
+    cell = [[self class] tableViewModel:tableViewModel
+                       cellForTableView:tableView
+                            atIndexPath:indexPath
+                             withObject:object];
+  }
+  return cell;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)mapObjectClass:(Class)objectClass toCellClass:(Class)cellClass {
+  [self.objectToCellMap setObject:cellClass forKey:objectClass];
 }
 
 
