96af1116653b083e7438a14a03465e40d2387ef6
jverkoey <jverkoey@gmail.com>
2011-11-24 09:28:15 -0800

    [core] Reset the in-memory cache state when removeAllObjects is called.
    We were not properly releasing the lru and we were not resetting the
    total pixel count to zero.
    
    Closes #117.

--- src/core/src/NIInMemoryCache.m
+++ src/core/src/NIInMemoryCache.m
@@ -341,7 +341,9 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)removeAllObjects {
   NI_RELEASE_SAFELY(_cacheMap);
+  NI_RELEASE_SAFELY(_lruCacheObjects);
   _cacheMap = [[NSMutableDictionary alloc] init];
+  _lruCacheObjects = [[NILinkedList alloc] init];
 }
 
 
@@ -437,6 +439,14 @@
     numberOfPixels *= [image scale];
   }
   return numberOfPixels;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)removeAllObjects {
+  [super removeAllObjects];
+
+  self.numberOfPixels = 0;
 }
 
 
