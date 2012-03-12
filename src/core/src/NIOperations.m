jverkoey/nimbus

commit 24af36e7209f2eb2f6da456e51564d5a25909985

[core] Clean up the error logic when an image 404s.

src/core/src/NIOperations.m
@@ -104,18 +104,17 @@
                                           returningResponse:&response
                                                       error:&networkError];
 
-    // If we get a 404 error, the network error will not everytime be filled with
-    // an error, so it will not fail with error, but will finish. We should check
-    // if in the response the status code is different from a 200 status code ok.
+    // If we get a 404 error then the request will not fail with an error, so only let successful
+    // responses pass.
     if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
-      NSHTTPURLResponse * httpResponse = (NSHTTPURLResponse *)response;
-      if ([httpResponse statusCode] != 200) {
-        networkError = [NSError errorWithDomain:@"nimbus" 
-                                           code:[httpResponse statusCode] 
+      NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse *)response;
+      if (httpResponse.statusCode >= 200 && httpResponse.statusCode < 300) {
+        networkError = [NSError errorWithDomain:NSURLErrorDomain
+                                           code:NSURLErrorResourceUnavailable
                                        userInfo:nil];
       }
     }
-      
+
     if (nil != networkError) {
       [self operationDidFailWithError:networkError];
 

commit 148d807192a06a7bc4f68c8c6ff6d46f58888d28

[core] Fix bug introduced in 24af36e7209f2eb2f6da456e51564d5a25909985.
All network requests were being marked as failed due to a logic
inversion.

Closes #173.

src/core/src/NIOperations.m
@@ -108,7 +108,7 @@
     // responses pass.
     if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
       NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse *)response;
-      if (httpResponse.statusCode >= 200 && httpResponse.statusCode < 300) {
+      if (httpResponse.statusCode < 200 || httpResponse.statusCode >= 300) {
         networkError = [NSError errorWithDomain:NSURLErrorDomain
                                            code:NSURLErrorResourceUnavailable
                                        userInfo:nil];