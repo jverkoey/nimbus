28a22e3fed4ecf0717ef9a37cc4786a2a0de203f
NukemHill <nukemhill@gmail.com>
2012-03-25 15:46:49 -0400

    replaced xcodeproj because of corruption issues.

---
+++ src/photos/src/GSHTest.h
@@ -0,0 +1,13 @@
+//
+//  GSHTest.h
+//  Nimbus
+//
+//  Created by Gregory Hill on 3/25/12.
+//  Copyright (c) 2012 Jeff Verkoeyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSHTest : NSObject
+
+@end
---
+++ src/photos/src/GSHTest.m
@@ -0,0 +1,13 @@
+//
+//  GSHTest.m
+//  Nimbus
+//
+//  Created by Gregory Hill on 3/25/12.
+//  Copyright (c) 2012 Jeff Verkoeyen. All rights reserved.
+//
+
+#import "GSHTest.h"
+
+@implementation GSHTest
+
+@end
