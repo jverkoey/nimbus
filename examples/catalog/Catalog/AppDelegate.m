f9a91345fd35295d342ff984b49c39a101dac91c
jverkoey <jverkoey@gmail.com>
2012-06-17 13:46:53 -0700

    [catalog] Deploy for iOS 5.0.

--- examples/catalog/Catalog/AppDelegate.m
+++ examples/catalog/Catalog/AppDelegate.m
@@ -20,6 +20,8 @@
 
 @implementation AppDelegate
 
+@synthesize window = _window;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   self.window.backgroundColor = [UIColor whiteColor];
