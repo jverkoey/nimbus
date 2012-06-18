127e1c750c4cc9f768dcd836e6566408d591e4a4
jverkoey <jverkoey@gmail.com>
2012-06-18 15:37:22 -0700

    [docs] Remove the Nimbus prefix from the index.

--- docs/cleannavtree.js
+++ docs/cleannavtree.js
@@ -1,1 +1,10 @@
+// Make the "Modules" section the root section.
 NAVTREE[0][2] = NAVTREE[0][2][0][2];
+
+// Remove the Nimbus prefix before every feature.
+for (var i = 0; i < NAVTREE[0][2].length; ++i) {
+  var name = NAVTREE[0][2][i][0];
+  if (name.indexOf('Nimbus ') == 0) {
+    NAVTREE[0][2][i][0] = name.substr('Nimbus '.length);
+  }
+}
