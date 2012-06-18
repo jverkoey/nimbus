6f48a375b4f61fb865531ab5c805b2f0e1cd6b46
jverkoey <jverkoey@gmail.com>
2012-06-18 16:27:17 -0700

    [docs] Language consistency. Aggressive tidying of the docs.

--- docs/cleannavtree.js
+++ docs/cleannavtree.js
@@ -8,3 +8,13 @@ for (var i = 0; i < NAVTREE[0][2].length; ++i) {
     NAVTREE[0][2][i][0] = name.substr('Nimbus '.length);
   }
 }
+
+function addAnchors(){
+  $.each($('.groupHeader'),function(index, value){
+    var identifier = $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^a-zA-Z0-9 \-]/g, '');
+    $('#box-anchors').append('<li><a href="#anchor-'+index+'">'+$(this).html()+'</a></li>');
+    $(this).prepend('<a name="'+identifier+'"></a>');
+  });
+}
+
+$(document).ready(addAnchors);
