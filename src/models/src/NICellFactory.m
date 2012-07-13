238cf1ef99655b5279bdeaba39848325166c32f7
jverkoey <jverkoey@gmail.com>
2012-07-13 00:23:49 -0700

    Revert "slightly better than previous one" This reverts commit 203dc0e43c4db7d2b413cb98de368b88f6d8fecb.
    Revert "nicellfactory support for coredata nsmanagedobject class"
    This reverts commit 156a2db9ee3feae1c485198104e23deae883d124.

--- src/models/src/NICellFactory.m
+++ src/models/src/NICellFactory.m
@@ -73,13 +73,6 @@
                         atIndexPath:(NSIndexPath *)indexPath
                          withObject:(id)object {
   UITableViewCell* cell = nil;
-  
-  id _object;
-  if ([object isKindOfClass:[NSManagedObject class]]) {
-    _object = [[NSClassFromString([[object entity] name]) alloc] init];
-  } else {
-    _object = object;
-  }
 
   // If this assertion fires then your app is about to crash. You need to either add an explicit
   // binding in a NICellFactory object or implement the NICellObject protocol on this object and
@@ -87,10 +80,11 @@
   NIDASSERT([object respondsToSelector:@selector(cellClass)]);
 
   // Only NICellObject-conformant objects may pass.
-  if ([_object respondsToSelector:@selector(cellClass)]) {
-    Class cellClass = [_object cellClass];
+  if ([object respondsToSelector:@selector(cellClass)]) {
+    Class cellClass = [object cellClass];
     cell = [self cellWithClass:cellClass tableView:tableView object:object];
   }
+
   return cell;
 }
 
