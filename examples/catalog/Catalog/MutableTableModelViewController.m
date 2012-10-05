6e0a0afd52652523cdb8bc8b5256ed2a24ab5401
jverkoey <jverkoey@gmail.com>
2012-10-04 18:32:15 -0700

    [catalog] Update the mutable model example to show how to update section indices as well.

--- examples/catalog/Catalog/MutableTableModelViewController.m
+++ examples/catalog/Catalog/MutableTableModelViewController.m
@@ -22,7 +22,9 @@
 //
 // What's going on in this file:
 //
-// This is a demo of using a NIMutableTableViewModel to modify the contents of a table model.
+// This is a demo of using a NIMutableTableViewModel to modify the contents of a table model. This
+// demo shows how to add a section of objects to the model, inform the table view that the section
+// has been added, and then recompile the section index.
 //
 // You will find the following Nimbus features used:
 //
@@ -49,8 +51,17 @@
   if ((self = [super initWithStyle:UITableViewStyleGrouped])) {
     self.title = @"Mutable Models";
 
+    // In order to be able to modify a model we must create an instance of NIMutableTableViewModel.
+    // This object differs from NITableViewModel in that it exposes methods for modifying the
+    // contents of the model, similarly to the differences between NSArray and NSMutableArray.
     _model = [[NIMutableTableViewModel alloc] initWithDelegate:(id)[NICellFactory class]];
 
+    // We are going to show how to recompile the section index so we provide the settings here.
+    [_model setSectionIndexType:NITableViewModelSectionIndexDynamic
+                    showsSearch:NO
+                   showsSummary:NO];
+
+    // By tapping this button we'll add a new section to the model.
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(didTapAddButton:)];
   }
   return self;
@@ -66,10 +77,37 @@
   return NIIsSupportedOrientation(toInterfaceOrientation);
 }
 
+- (NSString *)randomName {
+  NSMutableString *name = [[NSMutableString alloc] init];
+  for (NSInteger ix = 0; ix < arc4random_uniform(10) + 5; ++ix) {
+    [name appendFormat:@"%c", arc4random_uniform('z'-'a')+'a'];
+  }
+  return [name capitalizedString];
+}
+
 - (void)didTapAddButton:(UIBarButtonItem *)buttonItem {
-  NSIndexSet* indexSet = [self.model addSectionWithTitle:@"New section"];
-  NSArray* indexPaths = [self.model addObject:[NITitleCellObject objectWithTitle:@"A cell"]];
+  // We first create a new section in the model.
+  NSIndexSet* indexSet = [self.model addSectionWithTitle:[self randomName]];
+
+  // Then we create an array of objects that we want to add to this section.
+  NSMutableArray *objects = [NSMutableArray array];
+  for (NSInteger ix = 0; ix < arc4random_uniform(10) + 1; ++ix) {
+    [objects addObject:[NITitleCellObject objectWithTitle:[self randomName]]];
+  }
+
+  // The result of adding these objects is an array of index paths that can be used to ensure the
+  // visibility of the new objects.
+  NSArray* indexPaths = [self.model addObjectsFromArray:objects];
+
+  // Now that we've modified the model, we want to recompile the section index before notifying the
+  // table view of changes to the model.
+  [self.model updateSectionIndex];
+
+  // Tell the table view that we've added a new section and that it should use the default
+  // animation.
   [self.tableView insertSections:indexSet withRowAnimation:UITableViewRowAnimationAutomatic];
+
+  // Scroll the table view such that the last object is in view.
   [self.tableView scrollToRowAtIndexPath:indexPaths.lastObject atScrollPosition:UITableViewScrollPositionBottom animated:YES];
 }
 
