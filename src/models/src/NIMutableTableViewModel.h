5b1b5380eeeacebda29cad910add13dd639dd37e
jverkoey <jverkoey@gmail.com>
2012-10-10 10:55:18 -0700

    [models] Implement removeSectionAtIndex: in NIMutableViewViewModel.

--- src/models/src/NIMutableTableViewModel.h
+++ src/models/src/NIMutableTableViewModel.h
@@ -52,6 +52,7 @@ NSIndexSet* indexSet = [self.model addSectionWithTitle:@"New section"];
 
 - (NSIndexSet *)addSectionWithTitle:(NSString *)title;
 - (NSIndexSet *)insertSectionWithTitle:(NSString *)title atIndex:(NSUInteger)index;
+- (NSIndexSet *)removeSectionAtIndex:(NSUInteger)index;
 
 - (void)updateSectionIndex;
 
@@ -122,6 +123,14 @@ NSIndexSet* indexSet = [self.model addSectionWithTitle:@"New section"];
  *      @param index The index in the model at which to add the new section.
  *      @returns An index set with a single index representing the index of the new section.
  *      @fn NIMutableTableViewModel::insertSectionWithTitle:atIndex:
+ */
+
+/**
+ * Removes a section at the given index.
+ *
+ *      @param index The index in the model of the section to remove.
+ *      @returns An index set with a single index representing the index of the removed section.
+ *      @fn NIMutableTableViewModel::removeSectionAtIndex:
  */
 
 /** @name Updating the Section Index */
