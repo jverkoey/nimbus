74155a3105dd86d47e0db1f48cdd4a5851be3b4c
jverkoey <jverkoey@gmail.com>
2013-01-30 16:22:09 -0800

    [core] Add NIButtonUtilities.

---
+++ src/core/src/NIButtonUtilities.m
@@ -0,0 +1,80 @@
+//
+// Copyright 2011-2013 Jeff Verkoeyen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import "NIButtonUtilities.h"
+
+void NIApplyImageSelectorToButton(SEL selector, id target, UIButton* button) {
+  IMP method = [target methodForSelector:selector];
+  UIImage* image = nil;
+
+  image = method(target, selector, UIControlStateNormal);
+  [button setImage:image forState:UIControlStateNormal];
+
+  image = method(target, selector, UIControlStateHighlighted);
+  [button setImage:image forState:UIControlStateHighlighted];
+
+  image = method(target, selector, UIControlStateDisabled);
+  [button setImage:image forState:UIControlStateDisabled];
+
+  image = method(target, selector, UIControlStateSelected);
+  [button setImage:image forState:UIControlStateSelected];
+
+  UIControlState selectedHighlightState = UIControlStateSelected | UIControlStateHighlighted;
+  image = method(target, selector, selectedHighlightState);
+  [button setImage:image forState:selectedHighlightState];
+}
+
+void NIApplyBackgroundImageSelectorToButton(SEL selector, id target, UIButton* button) {
+  IMP method = [target methodForSelector:selector];
+  UIImage* image = nil;
+
+  image = method(target, selector, UIControlStateNormal);
+  [button setBackgroundImage:image forState:UIControlStateNormal];
+
+  image = method(target, selector, UIControlStateHighlighted);
+  [button setBackgroundImage:image forState:UIControlStateHighlighted];
+
+  image = method(target, selector, UIControlStateDisabled);
+  [button setBackgroundImage:image forState:UIControlStateDisabled];
+
+  image = method(target, selector, UIControlStateSelected);
+  [button setBackgroundImage:image forState:UIControlStateSelected];
+
+  UIControlState selectedHighlightState = UIControlStateSelected | UIControlStateHighlighted;
+  image = method(target, selector, selectedHighlightState);
+  [button setBackgroundImage:image forState:selectedHighlightState];
+}
+
+void NIApplyTitleColorSelectorToButton(SEL selector, id target, UIButton* button) {
+  IMP method = [target methodForSelector:selector];
+  UIImage* image = nil;
+
+  image = method(target, selector, UIControlStateNormal);
+  [button setTitleColor:image forState:UIControlStateNormal];
+
+  image = method(target, selector, UIControlStateHighlighted);
+  [button setTitleColor:image forState:UIControlStateHighlighted];
+
+  image = method(target, selector, UIControlStateDisabled);
+  [button setTitleColor:image forState:UIControlStateDisabled];
+
+  image = method(target, selector, UIControlStateSelected);
+  [button setTitleColor:image forState:UIControlStateSelected];
+
+  UIControlState selectedHighlightState = UIControlStateSelected | UIControlStateHighlighted;
+  image = method(target, selector, selectedHighlightState);
+  [button setTitleColor:image forState:selectedHighlightState];
+}
