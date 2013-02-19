6279044e72bc51be16f72cf92e895871965e0461
Max Metral <ma@x.com>
2013-02-19 11:38:55 -0500

    [core] Added extensions to NSInvocation to make them easier to create given a target and selector (primarily for use by upcoming CSS changes)

--- src/core/src/NSInvocation+NimbusCore.m
+++ src/core/src/NSInvocation+NimbusCore.m
@@ -2,12 +2,34 @@
 //  NSInvocation+NimbusCore.m
 //  Nimbus
 //
-//  Created by Metral, Max on 2/18/13.
 //  Copyright (c) 2013 Jeff Verkoeyen. All rights reserved.
 //
 
 #import "NSInvocation+NimbusCore.h"
+#import <objc/runtime.h>
+
+NI_FIX_CATEGORY_BUG(NSInvocation_NimbusCore)
 
 @implementation NSInvocation (NimbusCore)
+
++ (id)invocationWithTarget:(NSObject*)targetObject selector:(SEL)selector {
+  NSMethodSignature* sig = [targetObject methodSignatureForSelector:selector];
+  NSInvocation* inv = [NSInvocation invocationWithMethodSignature:sig];
+  [inv setTarget:targetObject];
+  [inv setSelector:selector];
+  return inv;
+}
+
++ (id)invocationWithClass:(Class)targetClass selector:(SEL)selector {
+  Method method = class_getInstanceMethod(targetClass, selector);
+  struct objc_method_description* desc = method_getDescription(method);
+  if (desc == NULL || desc->name == NULL)
+    return nil;
+  
+  NSMethodSignature* sig = [NSMethodSignature signatureWithObjCTypes:desc->types];
+  NSInvocation* inv = [NSInvocation invocationWithMethodSignature:sig];
+  [inv setSelector:selector];
+  return inv;
+}
 
 @end
