a512b3f90f32c461a768bf0fb13a8d97ac7ae41f
Max Metral <ma@x.com>
2013-09-08 23:38:20 -0400

    [css] return key, keyboard type, auto correct/capitalization support from css

--- src/css/src/UITextField+NIStyleable.m
+++ src/css/src/UITextField+NIStyleable.m
@@ -22,49 +22,53 @@ NI_FIX_CATEGORY_BUG(UITextField_NIStyleable)
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)applyStyleWithRuleSet:(NICSSRuleset *)ruleSet inDOM:(NIDOM *)dom
 {
-    [self applyTextFieldStyleBeforeViewWithRuleSet:ruleSet inDOM:dom];
-    [self applyViewStyleWithRuleSet:ruleSet inDOM:dom];
-    [self applyTextFieldStyleWithRuleSet:ruleSet inDOM:dom];
+  [self applyTextFieldStyleBeforeViewWithRuleSet:ruleSet inDOM:dom];
+  [self applyViewStyleWithRuleSet:ruleSet inDOM:dom];
+  [self applyTextFieldStyleWithRuleSet:ruleSet inDOM:dom];
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 -(void)applyStyleWithRuleSet:(NICSSRuleset*)ruleSet forPseudoClass:(NSString *)pseudo inDOM:(NIDOM*)dom
 {
-    if (ruleSet.hasTextKey) {
-        NIUserInterfaceString *nis = [[NIUserInterfaceString alloc] initWithKey:ruleSet.textKey];
-        [nis attach:self withSelector:@selector(setPlaceholder:)];
-    }
+  if (ruleSet.hasTextKey) {
+    NIUserInterfaceString *nis = [[NIUserInterfaceString alloc] initWithKey:ruleSet.textKey];
+    [nis attach:self withSelector:@selector(setPlaceholder:)];
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 -(void)applyTextFieldStyleBeforeViewWithRuleSet:(NICSSRuleset*)ruleSet inDOM:(NIDOM*)dom
 {
-    if (ruleSet.hasTextKey) {
-        NIUserInterfaceString *nis = [[NIUserInterfaceString alloc] initWithKey:ruleSet.textKey];
-        [nis attach:self withSelector:@selector(setTitle:forState:) forControlState:UIControlStateNormal];
-    }
+  if (ruleSet.hasTextKey) {
+    NIUserInterfaceString *nis = [[NIUserInterfaceString alloc] initWithKey:ruleSet.textKey];
+    [nis attach:self withSelector:@selector(setTitle:forState:) forControlState:UIControlStateNormal];
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 -(void)applyTextFieldStyleWithRuleSet:(NICSSRuleset*)ruleSet inDOM:(NIDOM*)dom
 {
-    if ([ruleSet hasTextColor]) { self.textColor = ruleSet.textColor; }
-    if ([ruleSet hasTextAlignment]) { self.textAlignment = ruleSet.textAlignment; }
-    if ([ruleSet hasFont]) { self.font = ruleSet.font; }
-    if ([ruleSet hasMinimumFontSize]) { self.minimumFontSize = ruleSet.minimumFontSize; }
-    if ([ruleSet hasAdjustsFontSize]) { self.adjustsFontSizeToFitWidth = ruleSet.adjustsFontSize; }
-    if ([ruleSet hasVerticalAlign]) { self.contentVerticalAlignment = ruleSet.verticalAlign; }
+  if ([ruleSet hasTextColor]) { self.textColor = ruleSet.textColor; }
+  if ([ruleSet hasTextAlignment]) { self.textAlignment = ruleSet.textAlignment; }
+  if ([ruleSet hasFont]) { self.font = ruleSet.font; }
+  if ([ruleSet hasMinimumFontSize]) { self.minimumFontSize = ruleSet.minimumFontSize; }
+  if ([ruleSet hasAdjustsFontSize]) { self.adjustsFontSizeToFitWidth = ruleSet.adjustsFontSize; }
+  if ([ruleSet hasVerticalAlign]) { self.contentVerticalAlignment = ruleSet.verticalAlign; }
+  if ([ruleSet hasReturnKeyType]) { self.returnKeyType = ruleSet.returnKeyType; }
+  if ([ruleSet hasKeyboardType]) { self.keyboardType = ruleSet.keyboardType; }
+  if ([ruleSet hasAutocapitalizationType]) { self.autocapitalizationType = ruleSet.autocapitalizationType; }
+  if ([ruleSet hasAutocorrectionType]) { self.autocorrectionType = ruleSet.autocorrectionType; }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 -(NSArray *)pseudoClasses
 {
-    static dispatch_once_t onceToken;
-    static NSArray *pseudos;
-    dispatch_once(&onceToken, ^{
-        pseudos = @[@":empty"];
-    });
-    return pseudos;
+  static dispatch_once_t onceToken;
+  static NSArray *pseudos;
+  dispatch_once(&onceToken, ^{
+    pseudos = @[@":empty"];
+  });
+  return pseudos;
 }
 
 @end
