18a37440ba4f55d7ed2c3ab2709a7d2cab2deed5
Jeff Verkoeyen <jverkoey@gmail.com>
2014-01-31 13:24:05 -0500

    [core] Corrected attribution in the header.

--- src/core/src/UIResponder+NimbusCore.h
+++ src/core/src/UIResponder+NimbusCore.h
@@ -1,6 +1,8 @@
 //
 // Copyright 2011-2014 Jeff Verkoeyen
 //
+// +currentFirstResponder originally written by Jakob Egger, adapted by Jeff Verkoeyen.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
