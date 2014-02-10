f0f80f05bf0ad98d032efef0aa7142a123f914df
Jeff Verkoeyen <jverkoey@gmail.com>
2014-02-10 12:51:34 -0500

    [catalog] Add an example clarifying how to chain actions and radio groups.
    Closes #456.

--- examples/catalog/Catalog/RadioGroupTableModelViewController.m
+++ examples/catalog/Catalog/RadioGroupTableModelViewController.m
@@ -22,13 +22,14 @@
 //
 // What's going on in this file:
 //
-// This is a demo of using a radio group with a table view.
+// This is a demo of using a radio group with a table view simultaneously with table actions.
 //
 // You will find the following Nimbus features used:
 //
 // [models]
 // NIRadioGroup
 // NITableViewModel
+// NITableViewActions
 // NICellFactory
 //
 // This controller requires the following frameworks:
@@ -44,16 +45,18 @@ typedef enum {
 } RadioGroup;
 
 @interface RadioGroupTableModelViewController () <NIRadioGroupDelegate>
-@property (nonatomic, retain) NITableViewModel* model;
-
-// In order to implement the radio group we must create a radio group object. This object will
-// maintain the state of the currently selected object. It will also handle all user interactions
-// and updating the visual state of the cell when the selection changes.
-@property (nonatomic, retain) NIRadioGroup* radioGroup;
 @end
 
-@implementation RadioGroupTableModelViewController
+@implementation RadioGroupTableModelViewController {
+  NITableViewModel* _model;
 
+  // In order to implement the radio group we must create a radio group object. This object will
+  // maintain the state of the currently selected object. It will also handle all user interactions
+  // and updating the visual state of the cell when the selection changes.
+  NIRadioGroup* _radioGroup;
+
+  NITableViewActions* _actions;
+}
 
 - (id)initWithStyle:(UITableViewStyle)style {
   if ((self = [super initWithStyle:UITableViewStyleGrouped])) {
@@ -67,12 +70,16 @@ typedef enum {
     // We want to be notified of changes to the radio group selection.
     _radioGroup.delegate = self;
 
+    // We'll demonstrate how to use a radio group and actions simultaneously.
+    _actions = [[NITableViewActions alloc] initWithTarget:self];
+
     // When we create the table contents we use the chaining pattern to create the table cell
     // object, map it in the radio group to an identifier, and then add the object to the array.
     // The radio group's mapObject:toIdentifier: method returns the given object which is what
     // allows us to use this pattern.
     NSArray* tableContents =
     [NSArray arrayWithObjects:
+     [_actions attachToObject:[NITitleCellObject objectWithTitle:@"Tap me"] tapSelector:@selector(didTapObject:)],
      [_radioGroup mapObject:[NITitleCellObject objectWithTitle:@"Option 1"] toIdentifier:RadioGroupOption1],
      [_radioGroup mapObject:[NITitleCellObject objectWithTitle:@"Option 2"] toIdentifier:RadioGroupOption2],
      [_radioGroup mapObject:[NITitleCellObject objectWithTitle:@"Option 3"] toIdentifier:RadioGroupOption3],
@@ -100,7 +107,7 @@ typedef enum {
 - (void)viewDidLoad {
   [super viewDidLoad];
   
-  self.tableView.dataSource = self.model;
+  self.tableView.dataSource = _model;
 
   // The radio group object implements a subset of the UITableViewDelegate methods and forwards all
   // delegate methods along. This allows us to insert the radio group into the delegate chain and
@@ -109,11 +116,29 @@ typedef enum {
   // Experiment:
   // Try removing this line. You will notice that the radio group no longer shows the current
   // selection or allows you to select objects in the group.
-  self.tableView.delegate = [self.radioGroup forwardingTo:self];
+  //
+  // Note:
+  // In order to support actions and radio groups on the same delegate you must chain them in the
+  // following order: actions -> radio group -> self.
+  //
+  // The actions objects enforces the concept that only cells with attached actions should be
+  // actionable. The radio group implements its own actions and assigns them afterward. Finally self
+  // can override any of this functionality in its implementation of the table view delegate.
+  //
+  // Implementing the chain in the opposite order would cause the actions object to "disable" the
+  // radio group. You can try this for yourself to see the effect.
+  self.tableView.delegate = [_actions forwardingTo:[_radioGroup forwardingTo:self]];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
   return NIIsSupportedOrientation(toInterfaceOrientation);
+}
+
+#pragma mark - User Actions
+
+- (BOOL)didTapObject:(id)object {
+  NSLog(@"Did tap object %@", object);
+  return YES;
 }
 
 #pragma mark - NIRadioGroupDelegate
