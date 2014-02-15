b1559d663ad570b299fa5575874c0d829d250751
Jeff Verkoeyen <jverkoey@gmail.com>
2014-02-15 01:25:20 -0400

    [docs] Update docs with new Doxygen 1.8.6-nimbuskit binary.

--- docs/gendocs.sh
+++
@@ -1,5 +0,0 @@
-doxygen docs/Doxyfile
-
-TEMP="$(mktemp /tmp/output.XXXXXXXXXX)"
-grep -v "Main Page" docs/output/html/navtree.js | grep -v "Class Index" | grep -v "Class Members" > $TEMP
-mv $TEMP docs/output/html/navtree.js
