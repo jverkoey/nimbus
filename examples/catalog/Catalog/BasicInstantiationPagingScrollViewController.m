445bcff2284867de8523e30d9612318ff414122f
Jeff Verkoeyen <jverkoey@gmail.com>
2014-02-15 16:41:36 -0400

    [catalog] Update copyright.

--- examples/catalog/Catalog/BasicInstantiationPagingScrollViewController.m
+++ examples/catalog/Catalog/BasicInstantiationPagingScrollViewController.m
@@ -1,6 +1,6 @@
 //
 // Copyright 2012 Manu Cornet
-// Copyright 2011-2012 Jeff Verkoeyen
+// Copyright 2011-2014 NimbusKit
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
