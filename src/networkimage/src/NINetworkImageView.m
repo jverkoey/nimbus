5ac5a3cdef54f7b77f7053efbd64e23d736967d2
Stephane Moore <stephane.moore@gmail.com>
2014-03-18 12:03:07 -0700

    Update type declarations for AFNetworking usage.

--- src/networkimage/src/NINetworkImageView.m
+++ src/networkimage/src/NINetworkImageView.m
@@ -353,7 +353,7 @@
          [self _didFailToLoadWithError:error];
       }];
 
-      [requestOperation setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
+      [requestOperation setDownloadProgressBlock:^(NSUInteger bytesRead, NSInteger totalBytesRead, NSInteger totalBytesExpectedToRead) {
           if ([self.delegate respondsToSelector:@selector(networkImageView:readBytes:totalBytes:)]) {
               [self.delegate networkImageView:self readBytes:totalBytesRead totalBytes:totalBytesExpectedToRead];
           }
