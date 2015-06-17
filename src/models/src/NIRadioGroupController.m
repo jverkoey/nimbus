c5833ae5e7bb346514e0837ceb82282431dc325b
randallli <randallli@google.com>
2015-06-17 15:41:05 -0400

    using the #define __IPHONE_9_0 to block

--- src/models/src/NIRadioGroupController.m
+++ src/models/src/NIRadioGroupController.m
@@ -75,8 +75,11 @@
 }
 #endif
 
-
+#if defined(__IPHONE_9_0) && __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_9_0
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+#else
+- (NSUInteger)supportedInterfaceOrientations {
+#endif
   return UIInterfaceOrientationMaskAllButUpsideDown;
 }
 
