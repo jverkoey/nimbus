74ca146caae12e93a0d411ef1e7a334e6b488c4e
Nobody <no-reply@google.com>
2022-01-24 13:28:51 -0800

    [Apple M1] Nimbus add support for M1 debugger break
    BEGIN_PUBLIC add support for arm macOS devices debugger breakpoint
    
    * Fix for jumping into debugger on M1 device with Simulator
    
    END_PUBLIC
    
    Tested if __builtin_debugtrap(); was working correctly
    by adding it to my project on M1 MacbookPro and when hitting
    that function in my App, it worked as expected by jumping
    in the debugger and then also able to continue the app
    after this.
    
    PiperOrigin-RevId: 423896988

--- src/core/src/NIDebuggingTools.h
+++ src/core/src/NIDebuggingTools.h
@@ -105,10 +105,8 @@ int NIIsInDebugger(void);
 #endif
 
 #if TARGET_IPHONE_SIMULATOR
-// We leave the __asm__ in this macro so that when a break occurs, we don't have to step out of
-// a "breakInDebugger" function.
 #define NIDASSERT(xx) { if (!(xx)) { NIDPRINT(@"NIDASSERT failed: %s", #xx); \
-if (NIDebugAssertionsShouldBreak && NIIsInDebugger()) { __asm__("int $3\n" : : ); } } \
+if (NIDebugAssertionsShouldBreak && NIIsInDebugger()) { __builtin_debugtrap(); } } \
 } ((void)0)
 #else
 #define NIDASSERT(xx) { if (!(xx)) { NIDPRINT(@"NIDASSERT failed: %s", #xx); \
